1cc0de7b5c715e0fda808e24f794708002ce91d5
TaherMarengoze <taher.marengoze@gmail.com>
2022-03-19 21:58:51 +0200

    Add Controller 'Categories'

---
+++ NorthwindAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NorthwindAPI.Models;
+
+namespace NorthwindAPI.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        /// <summary>
+        /// Get all the categories objects from the database.
+        /// </summary>
+        /// <returns>List of category object.</returns>
+        [HttpGet]
+        public HttpResponseMessage GetCategories()
+        {
+            //try
+            //{
+                using (NorthwindEntities northwindContext = new NorthwindEntities())
+                {
+                    List<ViewModels.Category> categories =
+                        (from modelCat in northwindContext.Categories
+                         select new ViewModels.Category()
+                         {
+                             CategoryID = modelCat.CategoryID,
+                             CategoryName = modelCat.CategoryName,
+                             Description = modelCat.Description,
+                             Image = modelCat.Picture,
+                         }).ToList();
+
+                    return
+                        Request.CreateResponse(HttpStatusCode.OK, categories);
+                }
+            //}
+            //catch (Exception ex)
+            //{
+            //    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            //}
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetCategory(int catId)
+        {
+            try
+            {
+                using (NorthwindEntities northwindContext = new NorthwindEntities())
+                {
+                    Category modelCat = northwindContext.Categories
+                        .FirstOrDefault(cat => cat.CategoryID == catId);
+
+                    ViewModels.Category category = new ViewModels.Category()
+                    {
+                        CategoryID = modelCat.CategoryID,
+                        CategoryName = modelCat.CategoryName,
+                        Description = modelCat.Description,
+                        //Image = modelCat.Picture,
+                    };
+
+                    if (category == null)
+                    {
+                        return
+                            Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            string.Format("Category with ID = {0}, not found", catId));
+                    }
+                    else
+                    {
+                        return
+                            Request.CreateResponse(HttpStatusCode.OK, category);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
---
+++ NorthwindAPI/ViewModels/Category.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace NorthwindAPI.ViewModels
+{
+    public class Category
+    {
+        private byte[] _image;
+
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Image
+        {
+            get => _image; set
+            {
+                _image = Offset78(value);
+            }
+        }
+
+        private byte[] Offset78(byte[] source)
+        {
+            byte[] offsetObject;
+
+            using (var ms = new MemoryStream())
+            {
+                int offset = 78;
+                ms.Write(source, offset, source.Length - offset);
+                Bitmap bmp = new Bitmap(ms);
+                using (MemoryStream jpegms = new MemoryStream())
+                {
+                    bmp.Save(jpegms, ImageFormat.Jpeg);
+                    offsetObject = jpegms.ToArray();
+                }
+            }
+            return offsetObject;
+        }
+    }
+}
